49c85d90f58094a75abac52920e3052e9a3d16bf
Mike Rhiney <rhineyman@gmail.com>
2021-04-03 14:33:04 -0700

    html pushing

---
+++ dist/info.js
@@ -0,0 +1,25 @@
+const header =`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">    
+</head>
+<body> 
+<section class="hero">
+        <div class="hero-body">
+          <p class="title">
+            My Team
+          </p>
+          <p class="subtitle">
+            "They're Awesome!" -Ghandi
+          </p>
+        </div>
+      </section>
+`
+const footer = `</body>
+</html>`
+
+module.exports={footer, header};
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
+const info = require('./dist/info.js')
 const employees = [];
 
 const questions = [
     {
         type: 'list',
-        message: 'What is the employees job title?',
-        name: 'title',
+        message: 'What is the employees role?',
+        name: 'role',
         choices: ['Manager', 'Engineer', 'Intern']
     },
 
@@ -32,8 +33,8 @@ const questions = [
         type: 'input',
         message: 'What is the Managers office number?',
         name: 'officeNumber',
-        when: ({title}) => {
-            if (title === 'Manager') {
+        when: ({role}) => {
+            if (role === 'Manager') {
                 return true;
             } else {
                 return false;
@@ -45,8 +46,8 @@ const questions = [
         type: 'input',
         message: 'What is the Engineers GitHub username?',
         name: 'github',
-        when: ({title}) => {
-            if(title === 'Engineer') {
+        when: ({role}) => {
+            if(role === 'Engineer') {
                 return true;
             } else {
                 return false;
@@ -58,8 +59,8 @@ const questions = [
         type: 'input',
         message: 'What University does the intern attend?',
         name: 'school',
-        when: ({title}) => {
-            if(title === 'Intern') {
+        when: ({role}) => {
+            if(role === 'Intern') {
                 return true;
             } else {
                 return false;
@@ -82,8 +83,98 @@ const promptQues = () => {
                 return promptQues();
             } else {            
             console.log(employees);
+            generateHtml();
             };
         })
 }
+
+function generateHtml () {
+    let finalHtml = `${info.header}`
+    employees.forEach(employee => {
+        if (employee.role === 'Manager') {            
+        
+    var card = `    <div class="card">        
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">${employee.firstName} ${employee.lastName}</p>
+          <p class="subtitle is-5">${employee.role}</p>
+          <p class="subtitle is-6">ID:${employee.id}</p>
+          <p class="subtitle is-6">Office:${employee.officeNumber}</p>
+        </div>
+      </div>
+  
+      <div class="content">
+        You can email me with any questions at <a>${employee.firstName}${employee.lastName}@freemail.com</a>.
+        <a href="#">#css</a> <a href="#">#responsive</a>            
+      </div>
+    </div>
+  </div>
+`} else if (employee.role === 'Engineer') {
+    var card = `    <div class="card">        
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">${employee.firstName} ${employee.lastName}</p>
+          <p class="subtitle is-5">${employee.role}</p>
+          <p class="subtitle is-6">ID:${employee.id}</p>
+          <p class="subtitle is-6">GitHub:${employee.github}</p>
+        </div>
+      </div>
+  
+      <div class="content">
+        You can email me with any questions at <a>${employee.firstName}${employee.lastName}@freemail.com</a>.
+        <a href="#">#css</a> <a href="#">#responsive</a>            
+      </div>
+    </div>
+  </div>
+`
+} else {
+    var card = `    <div class="card">        
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">${employee.firstName} ${employee.lastName}</p>
+          <p class="subtitle is-5">${employee.role}</p>
+          <p class="subtitle is-6">ID:${employee.id}</p>
+          <p class="subtitle is-6">School:${employee.school}</p>
+        </div>
+      </div>
+  
+      <div class="content">
+        You can email me with any questions at <a>${employee.firstName}${employee.lastName}@freemail.com</a>.
+        <a href="#">#css</a> <a href="#">#responsive</a>            
+      </div>
+    </div>
+  </div>
+`
+}
+finalHtml = finalHtml+card;
+    })
+        
+renderedHtml = finalHtml+info.footer;
+
+fs.writeFile('./dist/index.html', renderedHtml, (err,data) => {
+    if (err) {
+        throw err;
+    } console.log('successuflly created html');
+})       
+    
+};
 
 promptQues();
