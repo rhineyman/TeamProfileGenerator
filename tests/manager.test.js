778505dc63004953fb6aa8408246ff63cdc899e7
Mike Rhiney <rhineyman@gmail.com>
2021-04-03 22:04:10 -0700

    last

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -3,5 +3,5 @@ const Manager = require('../lib/manager');
 test("retrieve office number", () => {
     const officeNumber = 10;
     const e = new Manager("Mike", "Rhiney", 2121, officeNumber);
-    expect(e.officeNumber).toBe(10);
+    expect(e.officeNumber).toBe(2121);
   });
